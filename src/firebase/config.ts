ce7a8c4e28f96b5d2d2dbabfcbe3d43351331b13
thirumurugan <acetking24@outlook.com>
2025-11-08 16:20:22 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,6 @@
 export const firebaseConfig = {
   "projectId": "studio-8962118663-c0602",
   "appId": "1:666422755125:web:1067d7f1fa64e2b47d967c",
-  "apiKey": "AIzaSyDnp_717lVMdhZFNI8LFB2eWJ7StSnv0sk",
   "authDomain": "studio-8962118663-c0602.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "666422755125"
